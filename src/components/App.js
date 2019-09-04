ea83353d4ab4c659167caa20e553ce1c79cc8988
Claire Cassidy <clacassidy@gmail.com>
2019-09-03 17:12:54 -0700

    added magic title

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import MagicCreatures from '../container/MagicCreatures';
 
 export default function App() {
   return (
+    <>
+    <h1>WOW MAGIC</h1>
     <MagicCreatures />
+    </>
   );
-} 
+};
